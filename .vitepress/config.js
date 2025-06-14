fafc87d071be7d8324fc14217fc26855b3f79432
Saturn Connect <saturn_connect@proton.me>
2025-06-14 20:54:50 +0500

    Update config.js

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,6 +1,12 @@
 export default {
   title: 'Saturn Wiki',
-  description: 'Документация проекта Saturn',
+  description: 'Документация Saturn VPN. Инструкции по настройке VLESS VPN на любом устройстве.',
+  
+  head: [
+    ['link', { href: 'https://i.postimg.cc/76Db6sMN/favicon180x180.png', rel: 'apple-touch-icon', sizes: '180x180' }],
+    ['link', { href: 'https://i.postimg.cc/cJfrdyYH/favicon16x16.png', rel: 'icon', sizes: '16x16', type: 'image/png' }],
+    ['link', { href: 'https://i.postimg.cc/cHmC8RFG/favicon32x32.png', rel: 'icon', sizes: '32x32', type: 'image/png' }]
+  ],
   
   themeConfig: {
     logo: '/wiki.svg',
