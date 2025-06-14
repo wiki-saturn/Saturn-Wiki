43d566399d3d3b1fc69c98326a98d07148b4927e
Saturn Connect <saturn_connect@proton.me>
2025-06-14 18:40:44 +0500

    Create config.js

---
+++ .vitepress/config.js
@@ -0,0 +1,25 @@
+export default {
+  title: 'Saturn Wiki',
+  description: 'Документация проекта Saturn',
+  
+  themeConfig: {
+    nav: [
+      { text: 'Главная', link: '/' },
+      { text: 'Документация', link: '/guide/' }
+    ],
+    
+    sidebar: [
+      {
+        text: 'Начало работы',
+        items: [
+          { text: 'Введение', link: '/guide/' },
+          { text: 'Установка', link: '/guide/installation' }
+        ]
+      }
+    ],
+    
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/SaturnConnect/Saturn-Wiki' }
+    ]
+  }
+}
