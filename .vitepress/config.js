2104fcb358c39dd390ccbb674b07504b63769e68
Saturn Connect <saturn_connect@proton.me>
2025-07-29 23:34:12 +0500

    Update config.js

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -91,23 +91,19 @@ export default defineConfig({
       gtag('config', 'G-RQGCWMT85F');
     `],
     ['script', {}, `
-       (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-       m[i].l=1*new Date();
-       for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-       k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-       (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-    
-       ym(103338244, "init", {
-            clickmap:true,
-            trackLinks:true,
-            accurateTrackBounce:true,
-            webvisor:true
-       });
+       (function(m,e,t,r,i,k,a){
+           m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+           m[i].l=1*new Date();
+           for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+           k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+       })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=103528606', 'ym');
+
+       ym(103528606, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
     `],
-    ['noscript', {}, '<div><img src="https://mc.yandex.ru/watch/103338244" style="position:absolute; left:-9999px;" alt="" /></div>'],
+    ['noscript', {}, '<div><img src="https://mc.yandex.ru/watch/103528606" style="position:absolute; left:-9999px;" alt="" /></div>'],
     ['meta', { charset: 'utf-8' }],
     ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1' }],
-    ['meta', { name: 'yandex-verification', content: 'b50710c3c0486d68' }],
+    ['meta', { name: 'yandex-verification', content: 'da2c573fb9cd8183' }],
     ['meta', { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' }],
 
     ['meta', { name: 'robots', content: 'index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1' }],
